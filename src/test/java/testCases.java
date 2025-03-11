a3a2cbac7a6e806ab6540e55a0505b27cd603ae1
Md. Ashikur Rahman <48981854+ashik1074@users.noreply.github.com>
2025-03-11 12:01:19 +0600

    Update testCases.java

--- src/test/java/testCases.java
+++ src/test/java/testCases.java
@@ -15,8 +15,8 @@ public class testCases {
     void loginSystem(){
         driver.manage().window().maximize();
         driver.get("https://beta.rcms.gov.bd/login/");
-        driver.findElement(By.id("userId")).sendKeys("PUA.BEL@rcms.gov.be");
-        driver.findElement(By.id("loginPass")).sendKeys("Secr3t");
+        driver.findElement(By.id("userId")).sendKeys("P********@rcms.gov.be");
+        driver.findElement(By.id("loginPass")).sendKeys("******");
         driver.findElement(By.xpath("//*[@id=\"loginForm\"]/div[4]/button")).click();
         driver.findElement(By.xpath("//*[@id=\"designationForm\"]/div/div[2]/a")).click();
         driver.findElement(By.xpath("/html/body/div[1]/div[2]/div[1]/div/ul/li[3]/a")).click();
